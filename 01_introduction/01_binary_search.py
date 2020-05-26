7c5a22d26c7140c2be99095829387a448a8955b4
stevenchendan <qwe16165850@gmail.com>
2020-05-26 15:45:12 +1000

    add chapter 1 binary search python code

---
+++ 01_introduction/01_binary_search.py
@@ -0,0 +1,26 @@
+class Solution():
+  def binary_search(list, item):
+    
+    # handle edge case
+    if list == None or len(list) == 0:
+      return None
+    
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+      mid = (low + high) // 2
+      guess = list[mid]
+      if guess == item:
+        return mid
+      if guess < item:
+        low = mid + 1
+      else:
+        high = mid - 1
+      
+    return None
+
+
+test_array = [1,2,3,4,5,6]
+print(Solution.binary_search(test_array, 5))
+print(Solution.binary_search(test_array, -1))
