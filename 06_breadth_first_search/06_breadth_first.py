ca52357690936f41ce44f6dc337770f43a6dadc0
stevenchendan <qwe16165850@gmail.com>
2020-06-02 21:03:37 +1000

    implement breadth_first algorithm in chapter 6

---
+++ 06_breadth_first_search/06_breadth_first.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+#check if the name is ending with letter 'm'.
+def check_mango_seller(name):
+  return name[-1] == 'm'
+
+def search_mango_seller(name):
+  search_queue = deque()
+  search_queue += graph["you"]
+  while search_queue:
+    person = search_queue.popleft()
+    if check_mango_seller(person):
+      print(person + " is a mango seller!")
+      return True
+    else:
+      search_queue += graph[person]
+  return False
+
+
+if __name__ == "__main__":
+  search_mango_seller("you")
